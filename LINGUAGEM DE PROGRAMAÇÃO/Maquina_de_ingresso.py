e55d018429b2155348f3709a27ccc7cf2d2fbe64
Raquel Fonseca <raquelfonseca.rf@gmail.com>
2024-09-09 22:53:55 -0300

    maquina de ingresso finalizada

--- LINGUAGEM DE PROGRAMAÇÃO/Maquina_de_ingresso.py
+++ LINGUAGEM DE PROGRAMAÇÃO/Maquina_de_ingresso.py
@@ -1,59 +1,54 @@
-#Objetivos:
-    #A Máquina saúda a pessoa e pergunta a idade
-    #Se for menor de 18, impede a compra e encerra o atendimento
-    #Se for + 18 recomenda 3 filmes
-    # filme 1 - ação ; filme 2 romance ; filme 3 terror; 
-    # Caso escolha um valor diferente, avisa e pergunta de novo
-    # avisa quantos ingressos tem disponíveis de cada e a opção de voltar
-    # Caso escolha um valor diferente, avisa e pergunta de novo
-    # ao escolher a quantidade de ingressos, se disponível conclui a compra e agradece
+class Filmes: 
+    def __init__(self,codigo,genero,ingressos):
+        self.codigo = codigo
+        self.genero = genero
+        self.ingressos = ingressos
+    
+    def comprar_ingressos(self,qtd_compra):
+        while qtd_compra <= 0:
+            print(f"Quantidade errada")
+            qtd_compra = int(input("Quantos ingressos você gostaria de adquirir?"))     
+        while self.ingressos < qtd_compra:
+            print(f"Desculpe, mas há apenas {self.ingressos} disponíveis.")
+            qtd_compra = int(input("Quantos ingressos você gostaria de adquirir?"))
+        print(f"Pedido realizado com sucesso! {qtd_compra} ingresso(s) para filme de {self.genero}. Bom filme!")
 
+f1 = Filmes(1,"Ação",5)
+f2 = Filmes(2,"Comédia",1)
+f3 = Filmes(3,"Terror",15)
 
-
-
-def quantidade_Ingressos(genero,quantidade_disponivel, quantidade_escolhido):
-    print(f"Você escolheu {genero[genero-1]}, existem {quantidade_ingressos_disponiveis} ingressos disponíveis.")
-    quantidade_ingressos_disponiveis = quantidade_disponivel
-    quantidade_escolhido = int(input("Quantos ingressos você gostaria de adquirir?"))
-    if quantidade_escolhido > quantidade_ingressos_disponiveis:
-        print(f"Desculpe, o máximo é de {quantidade_ingressos_disponiveis} ingressos. Tente novamente")
-    else:
-         if quantidade_escolhido == 1:
-            print(f"Pedido realizado de {quantidade_escolhido} ingresso. Bom filme pra você!")
-         else:
-            print(f"Pedido realizado de {quantidade_escolhido} ingressos. Bom filme pra vocês")
+def filme_escolhido(filme):
+    print(f"Você escolheu filme de {filme.genero}.")
+    print(f"Este filme contém: {filme.ingressos} ingressos disponíveis.")
+    qtd_compra = int(input("Quantos ingressos deseja adquirir?"))
+    filme.comprar_ingressos(qtd_compra)
 
 def maquina():      
-    filme_escolhido = 0
     idade = int(input("Antes de começarmos com o atendimento, quantos anos você tem?"))
-
     if idade < 18:
             print(f"Desculpe, mas você não pode continuar a compra, até logo!")
             return
-
     else:
-            print("Temos 3 opções de filme para você:")
-            print("1 - Ação")
-            print("2 - Comédia")
-            print("1 - Terror")
-
-
-    while filme_escolhido == 0:
-        quantidade_ingressos_disponiveis = 0
-        filme_escolhido = int(input("Qual é o filme escolhido? ( digite apenas o número )"))
-        match filme_escolhido:
-            case 1: 
-                 filme_escolhido = 1
-                 quantidade_Ingressos(1,quantidade_ingressos_disponiveis,5)       
-            case 2: 
-                 filme_escolhido = 2
-                 quantidade_Ingressos(quantidade_ingressos_disponiveis,1)
-            case 3: 
-                 filme_escolhido = 3
-                 quantidade_Ingressos(quantidade_ingressos_disponiveis,15)
-            case _:
-                print("Opção inválida! Por favor, tente novamente.")
-                filme_escolhido = 0
-
+        print("Temos 3 opções de filme para você:")
+        print("1 - Ação")
+        print("2 - Comédia") 
+        print("3 - Terror")       
+        passou = False
+        while passou == False :
+            codigo = int(input("Qual é o filme escolhido? ( digite apenas o número )"))
+            match codigo:
+                case 1: 
+                    filme_escolhido(f1)
+                    passou = True
+                case 2: 
+                    filme_escolhido(f2)
+                    passou = True
+                case 3: 
+                    filme_escolhido(f3)
+                    passou = True
+                case _:
+                    print(f"Opção inválida! Por favor, tente novamente.")
+    
 print("Olá, bem vindo a Máquina de Ingressos")
+filmeCodigo = 0
 maquina()
